6cad6f55b1d5aaba0a7315f9a02bfe501be3fefd
Bazl1 <dazman3243243@gmail.com>
2024-12-22 19:04:37 +0200

    fix: storage url

--- src/shared/constants/config.ts
+++ src/shared/constants/config.ts
@@ -1,6 +1,6 @@
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}/api/`;
 export const APP_URL = process.env.NEXT_PUBLIC_APP_URL;
-export const STORAGE_URL = `${process.env.NEXT_PUBLIC_API_URL}/uploads/`;
+export const STORAGE_URL = `${process.env.NEXT_PUBLIC_API_URL}/api/uploads/`;
 
 export enum USER_ROLES {
     ADMIN = "admin",
